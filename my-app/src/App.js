edf4663a83eb9d98a5fce344d791b62a23d3b158
Rohittyagiii <tyagirohit698@gmail.com>
2024-09-24 07:50:55 -0700

    msg

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  const x=18;
+  return (
+    <>
+   <Header name="Rohit"/>
+   <Header name="Tyagi"/>
+   <Header name="Defender"/>
+    </>
+  );
+}
+
+export default App;
