f9cf8311734d3b2d493685f3087dbf89122f75e5
Rohittyagiii <tyagirohit698@gmail.com>
2024-09-25 08:57:21 -0700

    initial msg

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
+import  React, { useState } from 'react'; 
+import { useRef } from 'react';
+
 
 function App() {
-  const x=18;
-  return (
-    <>
-   <Header name="Rohit"/>
-   <Header name="Tyagi"/>
-   <Header name="Defender"/>
-    </>
-  );
+  const refElement = useRef("");
+  const [name,setName] = useState("Rohit")
+  console.log(refElement);
+
+  function Reset(){
+    setName("")
+    refElement.current.focus()
+  }
+   
+function handleInput(){
+  refElement.current.style.color="blue"
+  refElement.current.value="Tyagi"
+}
+
+    return (
+      <>
+      <h1>Learning useRef</h1>
+      <input ref={refElement} type='text' value={name} onChange={ (e)=> setName(e.target.value)} ></input>
+      <button onClick={Reset}>Reset</button>
+      <button onClick={handleInput}>Handle Event</button>
+      </>
+    )
 }
 
 export default App;
