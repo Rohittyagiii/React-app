365284125ce3efc17996f2de3722ea25eb1c42ca
Rohittyagiii <tyagirohit698@gmail.com>
2024-09-27 09:53:37 -0700

    msg display

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,22 @@
 import './App.css';
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import ChildA from './ChildA';
 
 function App() {
-
   const [add,setAdd] = useState(0);
-  const [subtract,setSubtract] = useState(100);
-
-   const Multiplication = useMemo( function multiply(){
-    console.log("***********");
-    return add*10;
-  },[add])
+const [count,setCount] = useState(0);
+  const Learning = useCallback(()=>{
+    //some operation
+  },[]);
    return(
     <div className='App'>
-      <h1>Learning useMemo</h1>
-      {Multiplication} <br/>
-      <button onClick={()=>{setAdd(add + 1)}}>Addition</button>
-      <span>{add}</span> <br  />
-      <button onClick={()=>{setSubtract(subtract - 1)}}>Subtraction</button>
-      <span>{subtract}</span>
+      <h1>Learning useCallback</h1>
+      <h1>{add}</h1>
+      <button  onClick={()=>{setAdd(add + 1)}}>Addition</button>
+      <h1>{count}</h1>
+      <button  onClick={()=>{setCount(count + 2)}}>Count</button>
     </div>
    ) ;  
-}
+  } 
 
 export default App;
---
+++ my-app/src/ChildA.js
@@ -0,0 +1,12 @@
+import React, { memo } from "react";
+
+function ChildA({Learning,count}){
+    console.log("Child Component");
+   return(
+    <>
+    
+    </>
+   )
+}
+
+export default memo(ChildA);
