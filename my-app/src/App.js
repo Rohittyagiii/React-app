06c43fce474edd53b13b0a5fbe6651ec5079895a
Rohittyagiii <tyagirohit698@gmail.com>
2024-10-01 08:57:09 -0700

    msg

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,13 @@
 import './App.css';
-import React, { useState } from 'react';
-
+import Form from './Form'
+ 
 function App() {
-    const [name,setName] = useState("");
-    const [password,setPassword] = useState("");
-   function handleChange(e){
-    // console.log(e.target.value);
-    //  setName(e.target.value);
-    // const capitalName = (e.target.value).toUpperCase();
-    //  setName(capitalName);
-
-    if(e.target.name == "firstName"){
-      // console.log(e.target.name);  
-      
-      const capitalName = (e.target.value).toUpperCase();
-      setName(capitalName);
-    }
-    else{
-      setPassword(e.target.value)
-    }
-
+  return(
+    <>
+    <Form/>
+    </>
+  );
    }
-   return(
-     <>
-     <form className='App'>
-     <h1>HTML FORMS</h1>
-      <label>First Name :</label><br/>
-      <input type='text' name='firstName' value={name} onChange={handleChange}/> <br/> <br/>
-      <label>Password :</label> <br/>
-      <input type='text' name='password' value={password} onChange={handleChange}/> <br/> <br/>
-      <button>Submit</button>
-     </form>
-     </>
-   )   
-  } 
+   
 
 export default App;
